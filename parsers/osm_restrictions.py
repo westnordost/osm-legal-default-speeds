648c402d5185e2e52a2c4f7a075706dfef26916a
Ian Wagner <ian.wagner@stadiamaps.com>
2019-07-21 20:07:25 +0900

    Run black

--- parsers/osm_restrictions.py
+++ parsers/osm_restrictions.py
@@ -3,63 +3,61 @@ from parsers.parse_utils import parser, ParseError
 
 def osm_speed_visitor(t):
     """Visitor function that outputs speeds in OSM tag syntax"""
-    if t.data in {'normal_speed', 'advisory_speed'}:
-        tag = 'maxspeed' if t.data == 'normal_speed' else 'maxspeed:advisory'
+    if t.data in {"normal_speed", "advisory_speed"}:
+        tag = "maxspeed" if t.data == "normal_speed" else "maxspeed:advisory"
 
         speed = osm_speed_visitor(t.children[0])
-        conditions = ' AND '.join([osm_speed_visitor(child) for child in t.children[1:]])
+        conditions = " AND ".join([osm_speed_visitor(child) for child in t.children[1:]])
         if conditions:
-            tag += ':conditional'
-            speed = f'{speed} @ ({conditions})'
+            tag += ":conditional"
+            speed = f"{speed} @ ({conditions})"
 
-        return {
-            tag: speed,
-        }
-    elif t.data == 'mph_speed':
-        return f'{t.children[0]} mph'
-    elif t.data == 'kph_speed':
-        return f'{t.children[0]}'
-    elif t.data == 'walk_speed':
-        return 'walk'
-    elif t.data == 'weight_restriction':
+        return {tag: speed}
+    elif t.data == "mph_speed":
+        return f"{t.children[0]} mph"
+    elif t.data == "kph_speed":
+        return f"{t.children[0]}"
+    elif t.data == "walk_speed":
+        return "walk"
+    elif t.data == "weight_restriction":
         return osm_speed_visitor(t.children[0])
-    elif t.data == 'qualified_restriction':
+    elif t.data == "qualified_restriction":
         return t.children[0].data
-    elif t.data == 'normal_weight':
-        return f'weightrating>{t.children[0]}'
-    elif t.data in {'qualified_weight_pre', 'qualified_weight_post'}:
-        qualifier_type = t.children[0 if t.data == 'qualified_weight_pre' else 1].data
-        if qualifier_type == 'empty':
-            weight_qualifier = 'emptyweight'
-        elif qualifier_type == 'capacity':
-            weight_qualifier = 'weightcapacity'
-        elif qualifier_type == 'trailer':
-            weight_qualifier = 'trailerweight'
+    elif t.data == "normal_weight":
+        return f"weightrating>{t.children[0]}"
+    elif t.data in {"qualified_weight_pre", "qualified_weight_post"}:
+        qualifier_type = t.children[0 if t.data == "qualified_weight_pre" else 1].data
+        if qualifier_type == "empty":
+            weight_qualifier = "emptyweight"
+        elif qualifier_type == "capacity":
+            weight_qualifier = "weightcapacity"
+        elif qualifier_type == "trailer":
+            weight_qualifier = "trailerweight"
         else:
             raise ParseError(f'Unexpected qualifier "{qualifier_type}"')
 
-        weight = t.children[1 if t.data == 'qualified_weight_pre' else 0]
+        weight = t.children[1 if t.data == "qualified_weight_pre" else 0]
 
-        return f'{weight_qualifier}>{weight}'
-    elif t.data == 'length_restriction':
-        return f'length>{t.children[0]}'
-    elif t.data == 'seat_restriction':
-        return f'seats>={t.children[0]}'
-    elif t.data == 'axle_restriction':
-        return f'axles>={t.children[0]}'
-    elif t.data == 'conditional_restriction':
-        if t.children[0].data == 'time':
+        return f"{weight_qualifier}>{weight}"
+    elif t.data == "length_restriction":
+        return f"length>{t.children[0]}"
+    elif t.data == "seat_restriction":
+        return f"seats>={t.children[0]}"
+    elif t.data == "axle_restriction":
+        return f"axles>={t.children[0]}"
+    elif t.data == "conditional_restriction":
+        if t.children[0].data == "time":
             return osm_speed_visitor(t.children[0].children[0])
         else:
             return t.children[0].data
-    elif t.data == 'interval':
+    elif t.data == "interval":
         return osm_speed_visitor(t.children[0])
-    if t.data == 'neg_interval':
-        return f'{t.children[0]}-{t.children[1]}'
-    elif t.data == 'pos_interval':
-        return f'{t.children[0]}+{t.children[1]}'
-    elif t.data == 'complex_time_span':
-        return f'({osm_speed_visitor(t.children[0])})-({osm_speed_visitor(t.children[1])})'
+    if t.data == "neg_interval":
+        return f"{t.children[0]}-{t.children[1]}"
+    elif t.data == "pos_interval":
+        return f"{t.children[0]}+{t.children[1]}"
+    elif t.data == "complex_time_span":
+        return f"({osm_speed_visitor(t.children[0])})-({osm_speed_visitor(t.children[1])})"
     else:
         raise ParseError(f'Unexpected token "{t}"')
 
@@ -72,7 +70,7 @@ def parse_speeds(s) -> dict:
     for speed in (osm_speed_visitor(speed_def) for speed_def in parse_tree.children):
         for k, v in speed.items():
             if k in result:
-                result[k] += f'; {v}'
+                result[k] += f"; {v}"
             else:
                 result[k] = v
 
--- test_speed_parser.py
+++ test_speed_parser.py
@@ -6,25 +6,37 @@ from parsers.osm_restrictions import parse_speeds
 @pytest.mark.parametrize(
     "data,expected",
     [
-        ("40", {'maxspeed': '40'}),  # Boring
-        ("40 mph", {'maxspeed': '40 mph'}),  # Add units
-        ("60mph, 40 mph (2t)", {'maxspeed': '60 mph', 'maxspeed:conditional': '40 mph @ (weightrating>2)'}),  # Add restriction
-        ("40 mph (2t, articulated)", {'maxspeed:conditional': '40 mph @ (weightrating>2 AND articulated)'}),  # Multiple conditions
-        ("60mph, 40 mph (2t), 20mph (6 axles)", {'maxspeed': '60 mph', 'maxspeed:conditional': '40 mph @ (weightrating>2); 20 mph @ (axles>=6)'}),  # Multiple restriction speeds
-        ("40 mph (2t trailer)", {'maxspeed:conditional': '40 mph @ (trailerweight>2)'}),
-        ("40 mph (articulated)", {'maxspeed:conditional': '40 mph @ (articulated)'}),
-        ("40 mph (trailer)", {'maxspeed:conditional': '40 mph @ (trailer)'}),
-        ("40 mph (caravan)", {'maxspeed:conditional': '40 mph @ (caravan)'}),
-        ("40 mph (wet)", {'maxspeed:conditional': '40 mph @ (wet)'}),
-        ("40 mph (6 axles)", {'maxspeed:conditional': '40 mph @ (axles>=6)'}),
-        ("40 mph (12 seats)", {'maxspeed:conditional': '40 mph @ (seats>=12)'}),
-        ("40 mph (empty 2t)", {'maxspeed:conditional': '40 mph @ (emptyweight>2)'}),
-        ("40 mph (capacity 2t)", {'maxspeed:conditional': '40 mph @ (weightcapacity>2)'}),
-        ("40 mph (10m)", {'maxspeed:conditional': '40 mph @ (length>10)'}),
-        ("40 mph (sunset-sunrise)", {'maxspeed:conditional': '40 mph @ (sunset-sunrise)'}),  # Time intervals
-        ("40 mph (sunset+01:00)", {'maxspeed:conditional': '40 mph @ (sunset+01:00)'}),
-        ("40 mph ((sunset+01:30)-(sunrise-01:30))", {'maxspeed:conditional': '40 mph @ ((sunset+01:30)-(sunrise-01:30))'}),
-        ("advisory: 130", {'maxspeed:advisory': '130'}),  # Advisory speed
+        ("40", {"maxspeed": "40"}),  # Boring
+        ("40 mph", {"maxspeed": "40 mph"}),  # Add units
+        (
+            "60mph, 40 mph (2t)",
+            {"maxspeed": "60 mph", "maxspeed:conditional": "40 mph @ (weightrating>2)"},
+        ),  # Add restriction
+        (
+            "40 mph (2t, articulated)",
+            {"maxspeed:conditional": "40 mph @ (weightrating>2 AND articulated)"},
+        ),  # Multiple conditions
+        (
+            "60mph, 40 mph (2t), 20mph (6 axles)",
+            {"maxspeed": "60 mph", "maxspeed:conditional": "40 mph @ (weightrating>2); 20 mph @ (axles>=6)"},
+        ),  # Multiple restriction speeds
+        ("40 mph (2t trailer)", {"maxspeed:conditional": "40 mph @ (trailerweight>2)"}),
+        ("40 mph (articulated)", {"maxspeed:conditional": "40 mph @ (articulated)"}),
+        ("40 mph (trailer)", {"maxspeed:conditional": "40 mph @ (trailer)"}),
+        ("40 mph (caravan)", {"maxspeed:conditional": "40 mph @ (caravan)"}),
+        ("40 mph (wet)", {"maxspeed:conditional": "40 mph @ (wet)"}),
+        ("40 mph (6 axles)", {"maxspeed:conditional": "40 mph @ (axles>=6)"}),
+        ("40 mph (12 seats)", {"maxspeed:conditional": "40 mph @ (seats>=12)"}),
+        ("40 mph (empty 2t)", {"maxspeed:conditional": "40 mph @ (emptyweight>2)"}),
+        ("40 mph (capacity 2t)", {"maxspeed:conditional": "40 mph @ (weightcapacity>2)"}),
+        ("40 mph (10m)", {"maxspeed:conditional": "40 mph @ (length>10)"}),
+        ("40 mph (sunset-sunrise)", {"maxspeed:conditional": "40 mph @ (sunset-sunrise)"}),  # Time intervals
+        ("40 mph (sunset+01:00)", {"maxspeed:conditional": "40 mph @ (sunset+01:00)"}),
+        (
+            "40 mph ((sunset+01:30)-(sunrise-01:30))",
+            {"maxspeed:conditional": "40 mph @ ((sunset+01:30)-(sunrise-01:30))"},
+        ),
+        ("advisory: 130", {"maxspeed:advisory": "130"}),  # Advisory speed
         ("junk", None),  # Obviously invalid
         ("40 mph ((2t)", None),  # Mismatched braces
         ("40 mph (2t))", None),  # Mismatched braces
@@ -35,6 +47,6 @@ def test_parser(data, expected):
     try:
         result = parse_speeds(data)
         assert result == expected
-    except:
+    except:  # noqa: E722
         if expected is not None:
             raise
