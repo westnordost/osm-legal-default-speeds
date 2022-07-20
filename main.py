242d3ababcc07e9e69f829319a9d8d0a0fe87772
Tobias Zwick <newton@westnordost.de>
2022-07-20 21:27:02 +0200

    add metainformation (license, revision id of the wiki page)

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ from parsers.osm_restrictions import parse_speeds
 from parsers.parse_utils import parse_road_types_table
 from parsers.parse_utils import parse_speed_table
 
+WIKI_URL = "https://wiki.openstreetmap.org/wiki/"
 WIKI_API_URL = "https://wiki.openstreetmap.org/w/api.php"
 WIKI_PAGE = "Default_speed_limits"
-
-
-def get_page_html(api_url: str, page_name: str) -> str:
-    res = requests.get(api_url, {"action": "parse", "page": page_name, "format": "json"})
-
-    return res.json()["parse"]["text"]["*"]
 
 
 if __name__ == "__main__":
     output_file_name = sys.argv[1] if len(sys.argv)>1 else "default_speeds.json"
 
-    html_string = get_page_html(WIKI_API_URL, WIKI_PAGE)
+    parsed = requests.get(WIKI_API_URL, {"action": "parse", "page": WIKI_PAGE, "format": "json"}).json()["parse"]
+    html_string = parsed["text"]["*"]
     soup = BeautifulSoup(html_string, "html.parser")
     speed_table = soup.find_all("table")[0]
     road_types = parse_road_types_table(soup.find_all("table")[1])
 
     result = parse_speed_table(speed_table, road_types, parse_speeds)
+    result["meta"] = {
+        "source": WIKI_URL + WIKI_PAGE,
+        "revision_id": parsed["revid"],
+        "license": "Creative Commons Attribution-ShareAlike 2.0 license",
+        "license_url": "https://wiki.openstreetmap.org/wiki/Wiki_content_license",
+    }
 
     for warning in result['warnings']:
         print(warning)
