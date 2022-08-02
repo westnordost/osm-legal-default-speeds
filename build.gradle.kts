fd4f714788342673db3a4d65bada68bd79e07c49
Tobias Zwick <newton@westnordost.de>
2022-08-03 01:32:20 +0200

    setup publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("multiplatform") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.dokka") version "1.7.10"
 }
 
 group = "de.westnordost"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -55,4 +58,60 @@ kotlin {
         val nativeMain by getting
         val nativeTest by getting
     }
+}
+
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("osm-legal-default-speeds")
+                description.set("Infer default legal speed limits from OpenStreetMap tags")
+                url.set("https://github.com/westnordost/osm-legal-default-speeds")
+                licenses {
+                    license {
+                        name.set("BSD 3-Clause License")
+                        url.set("https://raw.githubusercontent.com/westnordost/osm-legal-default-speeds/master/LICENSE.txt")
+                    }
+                }
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/westnordost/osm-legal-default-speeds/issues")
+                }
+                scm {
+                    connection.set("https://github.com/westnordost/osm-legal-default-speeds.git")
+                    url.set("https://github.com/westnordost/osm-legal-default-speeds")
+                }
+                developers {
+                    developer {
+                        id.set("westnordost")
+                        name.set("Tobias Zwick")
+                        email.set("osm@westnordost.de")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "oss"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                val ossrhUsername: String by project
+                val ossrhPassword: String by project
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
