9a7e6416a57a5f62002b6151cc2aab345a8cd1b7
Tobias Zwick <newton@westnordost.de>
2022-08-08 00:14:57 +0200

    fix hash params

--- demo/src/main/kotlin/HashParams.kt
+++ demo/src/main/kotlin/HashParams.kt
@@ -6,14 +6,18 @@ external fun encodeURIComponent(uriComponent: String): String
 var Location.hashParams: Map<String, String>
 get() = hash.substringAfter('#')
         .split('&')
-        .associate {
+        .mapNotNull {
             val splits = it.split('=', limit = 2)
             val key = decodeURIComponent(splits[0])
+            if (key.isEmpty()) return@mapNotNull null
             val value = if (splits.size > 1) decodeURIComponent(splits[1]) else ""
             key to value
-        }
+        }.toMap()
 set(value) {
-    hash = value.entries.joinToString(separator = "&", prefix = "#") { (key, value) ->
-        encodeURIComponent(key) + "=" + encodeURIComponent(value)
-    }
+    hash = value.entries.mapNotNull {
+        val key = encodeURIComponent(it.key)
+        val value = encodeURIComponent(it.value)
+        if (key.isEmpty()) return@mapNotNull null
+        if (value.isNotEmpty()) "$key=$value" else key
+    }.joinToString("&", "#")
 }
